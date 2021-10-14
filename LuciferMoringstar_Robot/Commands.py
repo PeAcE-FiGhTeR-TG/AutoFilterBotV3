48ac0ea6f161c19a847d0ce45f4c0f1829806de5
PeAcE-FiGhTeR-TG <92531486+PeAcE-FiGhTeR-TG@users.noreply.github.com>
2021-10-14 23:21:19 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🎖 DEPLOY YOURS 🎖', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('🔰 JOIN OUR GROUP 🔰', url='https://t.me/cineblasters')
                     ]
                     ]
                 await bot.send_cached_media(
