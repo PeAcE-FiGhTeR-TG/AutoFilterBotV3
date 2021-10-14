b8ff3fdd0530f43b5a156fec8ea883b2688c2708
PeAcE-FiGhTeR-TG <92531486+PeAcE-FiGhTeR-TG@users.noreply.github.com>
2021-10-14 23:39:51 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -115,10 +115,10 @@ async def start(bot, message):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[
-                InlineKeyboardButton("+𝗔𝗗𝗗 𝗠𝗘 𝗧𝗢 𝗬𝗢𝗨𝗥 𝗚𝗥𝗢𝗨𝗣",url="https://t.me/TGs_movie_Robot?startgroup=true")
+                InlineKeyboardButton("+𝗔𝗗𝗗 𝗠𝗘 𝗧𝗢 𝗬𝗢𝗨𝗥 𝗚𝗥𝗢𝗨𝗣", url="https://t.me/TGs_movie_Robot?startgroup=true")
                 ],[    
                 InlineKeyboardButton("Search Here", switch_inline_query_current_chat='')
-                InlineKeyboardButton("𝘾𝙧𝙚𝙖𝙩𝙚𝙧🕵️‍♂️",url="https://t.me/peace_fighter_TG")    
+                InlineKeyboardButton("𝘾𝙧𝙚𝙖𝙩𝙚𝙧🕵️‍♂️", url="https://t.me/peace_fighter_TG")    
                 ],[
                 InlineKeyboardButton("𝗛𝗘𝗟𝗣💡", callback_data="help"),
                 InlineKeyboardButton("𝗔𝗯𝗼𝘂𝘁 😎", callback_data="about")
