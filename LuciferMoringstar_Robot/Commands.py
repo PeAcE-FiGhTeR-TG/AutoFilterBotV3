3a7fb25fb15dd25bed8b903fc667100d66cd9a92
PeAcE-FiGhTeR-TG <92531486+PeAcE-FiGhTeR-TG@users.noreply.github.com>
2021-10-16 18:58:03 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -347,7 +347,12 @@ async def bot_info(bot, message):
     buttons = [
         [
             
-            InlineKeyboardButton('Deploy Video', url=f'{TUTORIAL}')
+            InlineKeyboardButton('🤖 𝖴𝗉𝖽𝖺𝗍𝖾𝗌', url='https://t.me/STMbOTsUPPORTgROUP'),
+            InlineKeyboardButton('🆁︎🅴︎🅿︎🅾︎', url='https://t.me/cineblasters')
+        ],
+        [
+            InlineKeyboardButton('🏠𝖧𝗈𝗆𝖾', url='callback_data="home"'),
+            InlineKeyboardButton('🔓𝖢𝗅𝗈𝗌𝖾', url='callback_data="close"')
         ]
         ]
     await message.reply(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
