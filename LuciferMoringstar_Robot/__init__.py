380e8b65a5a18d3cbfd117a9ea12b9cf5a869eaf
PeAcE-FiGhTeR-TG <92531486+PeAcE-FiGhTeR-TG@users.noreply.github.com>
2021-10-16 13:33:05 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -35,6 +35,6 @@ ABOUT = """
 ★ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ★ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴; [𝙼𝙾𝙽𝙶𝙾 𝙳𝙱](https://www.mongodb.com/cloud)
 ★ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: [𝙷𝙴𝚁𝙾𝙺𝚄](https://heroku.com/)
-  𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴; [𝙲𝙻𝙸𝙲𝙺 𝙼𝙴 👈](https://t.me/NOKIERUNNOIPPKITTUM)
+★ 𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴; [𝙲𝙻𝙸𝙲𝙺 𝙼𝙴 👈](https://t.me/NOKIERUNNOIPPKITTUM)
 ★ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: 𝚅3.𝟶 [ 𝙱𝙴𝚃𝙰 ]</b>
 """
