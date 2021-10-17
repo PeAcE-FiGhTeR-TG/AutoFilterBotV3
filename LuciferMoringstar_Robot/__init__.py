a681bd696f08f84fa16203d057671f49f4f75fa9
PeAcE-FiGhTeR-TG <92531486+PeAcE-FiGhTeR-TG@users.noreply.github.com>
2021-10-17 13:24:08 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,12 +30,13 @@ Bot Owner Only
 """
 
 ABOUT = """
-<b>★ 𝙼𝚈 𝙽𝙰𝙼𝙴: [𝙼𝚊𝚁𝚜𝙷𝚖𝙴𝙻𝙻𝙾](https;//t.me/TGs_movie_Robot)
-★ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁;[𝙿𝚎𝙰𝚌𝙴-𝙵𝚒𝙶𝚑𝚃𝚎𝚁-𝚃𝙶](https://t.me/peace_fighter_TG)
-★ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: [𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼](https://docs.pyrogram.org/)
-★ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: [𝙿𝚈𝚃𝙷𝙾𝙽 𝟹](https://www.python.org/)
-★ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴; [𝙼𝙾𝙽𝙶𝙾 𝙳𝙱](https://www.mongodb.com/cloud)
-★ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: [𝙷𝙴𝚁𝙾𝙺𝚄](https://heroku.com/)
-★ 𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴; [𝙲𝙻𝙸𝙲𝙺 𝙼𝙴 👈](https://t.me/NOKIERUNNOIPPKITTUM)
-★ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: 𝚅3.𝟶 [ 𝙱𝙴𝚃𝙰 ]</b>
+<b>♡︎ ᴍʏ ɴᴀᴍᴇ: [ᴍᴀʀsʜᴍᴇʟʟᴏ](https;//t.me/TGs_movie_Robot)
+♡︎ ᴄʀᴇᴀᴛᴏʀ:[𝙿ᴇ𝙰ᴄ𝙴-𝙵ɪ𝙶ʜ𝚃ᴇ𝚁-ᴛɢ](https://t.me/peace_fighter_TG)
+♡︎ ᴄʀᴇᴅɪᴛs: ᴇᴠᴇʀʏᴏɴᴇ ɪɴ ᴛʜɪs ᴊᴏᴜʀɴᴇʏ
+♡︎ ʟɪʙʀᴀʀʏ: [ᴘʏʀᴏɢʀᴀᴍ ᴀsʏɴᴄɪᴏ](https://docs.pyrogram.org/)
+♡︎ ʟᴀɴɢᴜᴀɢᴇ: [ᴘʏᴛʜᴏɴᴇ 3](https://www.python.org/)
+♡︎ ᴅᴀᴛᴀ ʙᴀsᴇ: [ᴍᴀɴɢᴏ ᴅʙ](https://www.mongodb.com/cloud)
+♡︎ ʙᴏᴛ sᴇʀᴠᴇʀ: [ʜᴇʀᴏᴋᴜ](https://heroku.com/)
+♡︎ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ: [ᴄʟɪᴄᴋ ᴍᴇ 👈](https://t.me/NOKIERUNNOIPPKITTUM)
+♡︎ ʙᴜɪʟᴅ sᴛᴀᴛᴜs: 𝚅3.𝟶 [ ʙᴇᴛᴀ ]</b>
 """
