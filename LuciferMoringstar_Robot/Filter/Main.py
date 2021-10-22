6a12200f8b4ce4d77d07fe31c493c7391fa51f70
PeAcE-FiGhTeR-TG <92531486+PeAcE-FiGhTeR-TG@users.noreply.github.com>
2021-10-22 14:54:31 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -326,7 +326,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🍿 𝐉𝐎𝐈𝐍 𝐎𝐔𝐑 𝐆𝐑𝐎𝐔𝐏 🍿', url='https://t.me/cineblasters')
+                        InlineKeyboardButton('🔄 Search Again 🔄', switch_inline_query_current_chat='')
                     ]
                     ]
                 
